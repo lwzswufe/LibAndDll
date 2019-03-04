a1c6b1a5242f78d636506c4aca6cda07ba70bd9c
lwzswufe <lwzswufe@foxmail.com>
2019-03-04 16:59:02 +0800

    为静态库生成Demo增加第二个头文件

---
+++ LibDemo/Person.cpp
@@ -0,0 +1,79 @@
+#include "stdafx.h"
+#include "Person.h"
+
+Person::Person(string name, Gender gender, Person* father, Person* mather)
+{
+    this->gender = gender;
+    this->name = name;
+    this->father = father;
+    this->mother = mather;
+}
+
+Person::~Person()
+{
+}
+
+vector<Person*> Person::get_children()
+{
+    return this->children;
+}
+
+Person* Person::get_father()
+{
+    return this->father;
+}
+
+Person* Person::get_mother()
+{
+    return this->mother;
+}
+
+Gender Person::get_gender()
+{
+    return this->gender;
+}
+
+void Person::add_child(Person *p)
+{
+    this->children.push_back(p);
+}
+
+Person * product_child(Person* father, Person* mother, string name, Gender gender)
+{
+    if (father == NULL || mother == NULL)
+    {
+        return NULL;
+    }
+    Person *person_ptr = new Person{ name, gender, father, mother };
+    father->add_child(person_ptr);
+    mother->add_child(person_ptr);
+    return person_ptr;
+}
+
+void Show_Person(Person* p)
+{
+    if (p == NULL)
+        cout << "Person:NULL" << endl;
+    cout << "Name:" << p->name << endl;
+
+    cout << "Father:";
+    if (p->get_father() == NULL)
+        cout << "NULL" << endl;
+    else
+        cout << p->get_father()->name << endl;
+
+    cout << "Mother:";
+    if (p->get_mother() == NULL)
+        cout << "NULL" << endl;
+    else
+        cout << p->get_mother()->name << endl;
+
+    vector<Person*> children = p->get_children();
+    cout << "Children:";
+    if (children.empty())
+        cout << "NULL";
+    for (auto child : children)
+        cout << child->name << ", ";
+    cout << endl;
+
+}
--- LibTest/LibTest.cpp
+++ LibTest/LibTest.cpp
@@ -1,10 +1,11 @@
-﻿// LibTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+// LibTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 // 将LibDemo.lib添加到 链接器 附加依赖项
 // 将LibDemo.lib的目录 添加到 连接器 常规 附加库目录
 // 将Vector.h 添加到资源文件 
 // 将Vector.h的目录添加到  VC++目录包含目录
 #include "pch.h"
 #include "Vector.h"
+#include "Person.h"
 #include <iostream>
 using std::cout; using std::endl;
 int main()
@@ -19,5 +20,11 @@ int main()
     Vector v3 = v1 + v2;  
     v3.show();
 
+    Person p, p1{ "Bob", Gender::male }, p2{ "Ace", Gender::female };
+    Person *pp3 = product_child(&p1, &p2, "baby", Gender::male);
+    Show_Person(pp3);
+    Show_Person(&p1);
+    Show_Person(&p2);
+    Show_Person(&p);
     return 0;
 }
